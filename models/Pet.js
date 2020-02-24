ff486aa9ee04c20b340780241432f6cb7d4435b2
AhmedMajoul <60159015+AhmedMajoul@users.noreply.github.com>
2020-02-24 16:04:04 +0100

    first commit

---
+++ models/Pet.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const PetSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  picture: {
+    type: String
+  },
+  dateBirth: {
+    type: Date
+  },
+  date:{
+      type:Date,
+      default:Date.now
+  },
+  species:{
+    type:String,
+    required:true
+  },
+  race:{
+    type:String,
+  },
+  userId:{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user'
+  }
+});
+
+module.exports = Pet = mongoose.model('pet', PetSchema);
