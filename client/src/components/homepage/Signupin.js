2813e97a45eae6650bb6901272b1c7955da385a4
yahyaoui-mohamed-bayrem <y.bayrem@gmail.com>
2020-02-28 23:08:25 +0100

    test

--- client/src/components/homepage/Signupin.js
+++ client/src/components/homepage/Signupin.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Button, ButtonGroup } from 'reactstrap';
-import Signupin from './Signupin.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import './Signupin.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 const SplitButton = (props) => {
